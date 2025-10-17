5b0daad6950c36bb4978925788d0d10e3b2801e8
Alexsandro Samuel <alexsandro.s.samuel@gmail.com>
2025-10-17 02:58:21 +0000

    VERIFIQUE O TRÁFEGO E INTERAÇÃO DO CHECKOUT, ESTÁ DEMORANDO

--- src/lib/stripe-client.ts
+++ src/lib/stripe-client.ts
@@ -2,15 +2,15 @@
 import { Stripe, loadStripe } from '@stripe/stripe-js';
 
 let stripePromise: Promise<Stripe | null>;
+const publishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
+
+if (publishableKey) {
+  stripePromise = loadStripe(publishableKey);
+} else {
+  console.error("Stripe publishable key (NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) is not set in environment variables.");
+  stripePromise = Promise.resolve(null);
+}
 
 export const getStripe = () => {
-  if (!stripePromise) {
-    const publishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
-    if (!publishableKey) {
-      console.error("Stripe publishable key (NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) is not set in environment variables.");
-      return Promise.resolve(null);
-    }
-    stripePromise = loadStripe(publishableKey);
-  }
   return stripePromise;
 };
