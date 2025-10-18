ad8cd64bc8ea6a5041614f76c4a200f327df065f
Alexsandro Samuel <alexsandro.s.samuel@gmail.com>
2025-10-18 03:58:32 +0000

    mesmmos erros, não podemos excluir a parte do projeto que esta levando a

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,11 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
